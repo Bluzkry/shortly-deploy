5f4cde29f4e2e9e5ce4f76816f0f7b2dbea3d79e
MKS-Student <mks.student@hackreactor.com>
2017-03-02 17:57:43 -0800

    Finished with refactoring

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,45 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// url
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function() {
+//     this.on('creating', function(model, attrs, options) {
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+var urlSchema = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  timestamps: {type: Date, default: Date.now}
 });
+
+urlSchema.pre('save', function(next) {
+  var user = this;
+  var shasum = crypto.createHash('sha1');
+  shasum.update(user.url);
+  user.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+
+var Link = mongoose.model('Link', urlSchema);
+
+// var Link = new urls({
+// })
 
 module.exports = Link;
